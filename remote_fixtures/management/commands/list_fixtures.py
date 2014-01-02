61d50ea7640fb7ec9c8a95de34910b789828a885
Jacob Haslehurst <jacob@haslehurst.net>
2014-01-02 15:23:50 +1100

    adding list_fixtures management command

---
+++ remote_fixtures/management/commands/list_fixtures.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+from datetime import datetime
+import dateutil.parser
+from django.core.management.base import BaseCommand
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from clint.textui import columns
+
+from remote_fixtures.utils import S3Mixin, humanize_filesize
+
+
+class Command(BaseCommand, S3Mixin):
+    def handle(self, *args, **options):
+        bucket = self.get_bucket()
+
+        fixtures = list(bucket.list('fixture_'))
+        for fixture in fixtures:
+            fixture.last_modified_dt = dateutil.parser.parse(fixture.last_modified)
+
+        fixtures.sort(key=lambda x: x.last_modified_dt)
+
+        for fixture in fixtures:
+            print columns(
+                [fixture.name, 40],
+                [humanize_filesize(fixture.size), 10],
+                [fixture.last_modified_dt.strftime('%e %b %G'), 15],
+                [naturaltime(fixture.last_modified_dt), 30],
+            )
