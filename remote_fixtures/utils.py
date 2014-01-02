9f273d69e396bba66c06c0e9eb7dac9cf4e9af99
Jacob Haslehurst <jacob@haslehurst.net>
2014-01-02 15:20:09 +1100

    adding humanize_filesize utility function

--- remote_fixtures/utils.py
+++ remote_fixtures/utils.py
@@ -8,3 +8,13 @@ class S3Mixin(object):
             self.bucket = conn.get_bucket(settings.REMOTE_FIXTURE_BUCKET)
 
         return self.bucket
+
+
+def humanize_filesize(bytes):
+    for x in [' bytes','KB','MB','GB']:
+        if bytes == 0:
+            return '0 bytes'
+        if bytes < 1024.0 and bytes > -1024.0:
+            return "%3.1f%s" % (bytes, x)
+        bytes /= 1024.0
+    return "%3.1f%s" % (bytes, 'TB')
