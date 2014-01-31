362cbf604dddd4444b5543b972562aa819f1241e
Jacob Haslehurst <jacob@haslehurst.net>
2014-02-01 00:13:04 +1100

    version 0.4.0 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='remote-fixtures',
-    version='0.3.0',
+    version='0.4.0',
     description='Django remote fixture loading',
     author='Gizmag',
     author_email='tech@gizmag.com',
